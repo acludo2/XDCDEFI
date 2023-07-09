e70cdd94a67fb94257c429ef1802192fff49d081
cesar angulo <cesarangulo@cesars-MacBook-Air-2.local>
2023-07-08 20:10:26 -0700

    stuff

--- pair.js
+++ pair.js
@@ -1,181 +1,49 @@
 const web3 = new Web3(window.ethereum);
 
-// Contract ABI and address
-const oracleFactoryABI = [
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "address",
-				"name": "oracleAddress",
-				"type": "address"
-			}
-		],
-		"name": "OracleCreated",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"name": "allOracles",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_name",
-				"type": "string"
-			}
-		],
-		"name": "createOracle",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "oracleAddress",
-				"type": "address"
-			},
-			{
-				"internalType": "int256",
-				"name": "data",
-				"type": "int256"
-			}
-		],
-		"name": "fulfillOracleRequest",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "getAllOracles",
-		"outputs": [
-			{
-				"internalType": "address[]",
-				"name": "",
-				"type": "address[]"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"name": "getOracle",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-];
-
-const oracleABI = [
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "_name",
-				"type": "string"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "constructor"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "int256",
-				"name": "_price",
-				"type": "int256"
-			}
-		],
-		"name": "fulfillRequest",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "name",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "owner",
-		"outputs": [
-			{
-				"internalType": "address",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "price",
-		"outputs": [
-			{
-				"internalType": "int256",
-				"name": "",
-				"type": "int256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	}
-];
 
 // Replace with your contract addresses
-const oracleFactoryAddress = "0xd6887f70d46a7253dAE1247C1eA38f0bD9DFEcda";
 
 // Contract instances
-const oracleFactoryContract = new web3.eth.Contract(oracleFactoryABI, oracleFactoryAddress);
 
 // Accounts array
 let accounts = [];
 
 
+async function showConnect() {
+  accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  console.log("Connected: ", accounts[0]);
+
+  //let balance = await web3.eth.getBalance(accounts[0]).then(console.log);
+  web3.eth.getChainId().then(console.log);
+  document.getElementById("connectButton").innerHTML = accounts[0].substr(0, 2) + "..." + accounts[0].substr(-4);
+  document.getElementById("connectButton").style.backgroundColor = '#1e2e9e';
+  balance = web3.utils.fromWei(await web3.eth.getBalance(accounts[0]), 'ether');
+  //document.getElementById("depositMaxButton").innerHTML = "Deposit Max XDC (max is:" + balance;
+  window.ethereum.on('accountsChanged', function (accounts) {
+      // Time to reload your interface with accounts[0]!
+      console.log("Account changed: ", accounts[0]);
+
+  });
+
+}
+async function connect() {
+  accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  showConnect()
+}
+
+document.getElementById('connectButton').addEventListener('click', connect);
+
+
 window.onload = function() {
+  let oracleFactoryContract;
+
+fetch('oracleFactory.json')
+  .then(response => response.json())
+  .then(oracleFactory => {
+    oracleFactoryContract = new web3.eth.Contract(oracleFactory.abi, oracleFactory.address);
+  })
+  .catch(error => console.error(error));
+
   fetch('tokens.json')
     .then(response => response.json())
     .then(tokens => {
@@ -200,16 +68,55 @@ window.onload = function() {
     })
     .catch(error => console.error(error));
 
-  document.getElementById("createPairButton").addEventListener("click", function() {
-    const fromTokenAddress = $('#fromTokenDropdown').dropdown('get value');
-    const toTokenAddress = $('#toTokenDropdown').dropdown('get value');
 
-    const fromOracleAddress = $('#fromTokenDropdown').dropdown('get item').data('oracle-address');
-    const toOracleAddress = $('#toTokenDropdown').dropdown('get item').data('oracle-address');
+
+let routerContract;
+
+fetch('router.json')
+  .then(response => response.json())
+  .then(router => {
+    routerContract = new web3.eth.Contract(router.abi, router.address);
+  })
+  .catch(error => console.error(error));
+
+
+
+
+
+  document.getElementById("createPairButton").addEventListener("click", function() {
+    const fromTokenAddress = document.querySelector("#fromTokenDropdown input[name='fromToken']").value;
+    const toTokenAddress = document.querySelector("#toTokenDropdown input[name='toToken']").value;
+
+    // Check if same token is selected in both dropdowns
+    if(fromTokenAddress === toTokenAddress) {
+      alert('Cannot select the same token for both sides of the pair');
+      return;
+    }
+
+    const fromOracleAddress = document.querySelector(`#fromTokenDropdown .menu .item[data-value='${fromTokenAddress}']`).dataset.oracleAddress;
+    const toOracleAddress = document.querySelector(`#toTokenDropdown .menu .item[data-value='${toTokenAddress}']`).dataset.oracleAddress;
 
     // Now you have your selected token addresses and their corresponding oracle addresses
     // You can use these to interact with your smart contracts
     console.log(fromTokenAddress, toTokenAddress, fromOracleAddress, toOracleAddress);
-  });
-};
 
+    const amountADesired = web3.utils.toWei('1', 'ether'); // Example: 1 tokenA
+    const amountBDesired = web3.utils.toWei('1', 'ether'); // Example: 1 tokenB
+
+    routerContract.methods.addLiquidity(
+      fromTokenAddress,
+      toTokenAddress,
+      amountADesired,
+      amountBDesired,
+      accounts[0] // destination address
+    ).send({ from: accounts[0] })
+      .on('receipt', (receipt) => {
+        console.log(receipt);
+      })
+      .on('error', (error) => {
+        console.error(error);
+      });
+  });
+
+
+};
